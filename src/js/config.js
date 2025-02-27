5ed4c9767a20690c2a96cada676d4a6fb811e703
hammey07 <hammey07.hs@gmail.com>
2025-02-27 20:10:23 +0000

    API token updated

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '7880e5dc-08d7-450e-8493-4db82180ae93';
+export const KEY = '4c3c7f3b-4f7e-485d-ae7f-9e3d65df568b';
 export const MODAL_CLOSE_SEC = 2.5;
